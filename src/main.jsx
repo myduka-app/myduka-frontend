cd822a3d7f94e63d95786d8dc2834d4c52d488fb
MaginaBrian <brianojuk@gmail.com>
2025-07-31 10:19:27 +0300

    updatinhg routing routes lkeading to respective dashboards for users

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+import MerchantRegister from './components/MerchantRegister.jsx'
+import MerchantDashboard from './components/MerchantDashboard.jsx'
+import AdminDashboard from './components/AdminDashboard.jsx'
+import ClerkDashboard from './components/ClerkDashboard.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register-merchant" element={<MerchantRegister />} />
+        <Route
+          path="/merchant-dashboard"
+          element={
+            <ProtectedRoute>
+              <MerchantDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin-dashboard"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/clerk-dashboard"
+          element={
+            <ProtectedRoute>
+              <ClerkDashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
